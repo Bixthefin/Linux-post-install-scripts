2518932e84dee2d1539e173dc2d53d6986564c32
Bixthefin <114880614+Bixthefin@users.noreply.github.com>
2025-02-27 20:35:14 +0000

    initial Debian support
    This script has not been tested currently and is only the first version that is very basic

---
+++ Linux script-Debian-Based.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# After install script for Linux
+
+echo "Starting installation of applications..."
+
+sudo apt install -y firefox
+sudo apt install -y steam
+sudo apt install -y discord
+sudo apt install -y prism-launcher
+sudo apt install -y vlc
+sudo apt install -y gnome-tweaks
+sudo apt install -y neofetch
+
+# All installs finished
+neofetch
+
+echo "All applications have been successfully installed!"
+echo "Thank you for using this Linux script from Bixthefin."
